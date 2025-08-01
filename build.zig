725967336dbcba03f1d3f9d6bf479da653370962
Beyley Cardellio <ep1cm1n10n123@gmail.com>
2025-08-01 01:43:19 -0700

    client + gpu: Initial Commit
    Basic "hello world" window with client, pull in SDL_gpu and get Vulkan backend compiling.

---
+++ build.zig
@@ -0,0 +1,77 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const use_llvm = b.option(bool, "use_llvm", "Whether to compile using LLVM") orelse true;
+    const use_lld = b.option(bool, "use_lld", "Whether to link using LLD") orelse true;
+
+    const sdl3_dep = b.dependency("sdl3", .{
+        .target = target,
+        .optimize = optimize,
+        .c_sdl_preferred_linkage = .dynamic,
+    });
+
+    const sdl3_mod = sdl3_dep.module("sdl3");
+
+    const gpu_root = b.path("gpu/");
+    const gpu_mod = b.addModule("gpu", .{
+        .target = target,
+        .optimize = optimize,
+
+        .root_source_file = gpu_root.path(b, "gpu.zig"),
+
+        .imports = &.{
+            .{ .name = "sdl3", .module = sdl3_mod },
+        },
+    });
+
+    gpu_mod.addCSourceFiles(.{
+        .root = gpu_root,
+        .language = .c,
+        .files = &.{"hashtable/SDL_hashtable.c"},
+    });
+
+    gpu_mod.addCSourceFiles(.{
+        .root = gpu_root,
+        .files = &.{
+            "SDL_gpu.c",
+            "vulkan/SDL_gpu_vulkan.c",
+        },
+        .language = .c,
+    });
+    gpu_mod.addCMacro("SDL_GPU_VULKAN", "1");
+
+    const gloobie_mod = b.addModule("gloobie", .{
+        .root_source_file = b.path("client/main.zig"),
+
+        .optimize = optimize,
+        .target = target,
+
+        .imports = &.{
+            .{ .name = "sdl3", .module = sdl3_mod },
+            .{ .name = "gpu", .module = gpu_mod },
+        },
+    });
+
+    const gloobie_exe = b.addExecutable(.{
+        .name = "gloobie",
+        .root_module = gloobie_mod,
+        .use_lld = use_lld,
+        .use_llvm = use_llvm,
+        .version = .{
+            .major = 0,
+            .minor = 0,
+            .patch = 1,
+            .pre = "alpha",
+        },
+    });
+
+    b.installArtifact(gloobie_exe);
+
+    const run_step = b.step("run", "Runs the gloobie executable");
+
+    const gloobie_exe_run = b.addRunArtifact(gloobie_exe);
+    run_step.dependOn(&gloobie_exe_run.step);
+}
---
+++ client/main.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const sdl3 = @import("sdl3");
+
+pub fn main() !void {
+    var debug_alloc_impl: std.heap.DebugAllocator(.{}) = .init;
+
+    const gpa = if (std.debug.runtime_safety) debug_alloc_impl.allocator() else std.heap.smp_allocator;
+
+    try sdl3.setMemoryFunctionsByAllocator(gpa);
+
+    try sdl3.init(.{
+        .video = true,
+    });
+    defer sdl3.shutdown();
+
+    const properties_result = try sdl3.video.Window.initWithProperties(.{
+        .resizable = true,
+        .mouse_grabbed = true,
+        .width = 1600,
+        .height = 900,
+        .title = "gloobie",
+
+        .external_graphics_context = true,
+        .vulkan = true, // TODO: support other graphics backends
+    });
+    const window = properties_result.window;
+    properties_result.properties.deinit();
+
+    defer window.deinit();
+
+    var run: bool = true;
+    while (run) {
+        if (sdl3.events.poll()) |event| {
+            switch (event) {
+                .quit => {
+                    run = false;
+                },
+                else => {},
+            }
+        }
+
+        // TODO: create GPU context and submit frames
+    }
+}
