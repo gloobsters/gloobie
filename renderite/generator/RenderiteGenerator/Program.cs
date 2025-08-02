b97b4d664b1307a08a2960944175837746d9a721
jvyden <jvyden@jvyden.xyz>
2025-08-02 04:12:27 -0400

    rendrite: Simple enum type generation via C# reflection

---
+++ renderite/generator/RenderiteGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+using RenderiteGenerator;
+
+Parser.Default.ParseArguments<GeneratorOptions>(args)
+    .WithParsed(o =>
+    {
+        using Generator generator = new(o);
+        generator.Run();
+    });
