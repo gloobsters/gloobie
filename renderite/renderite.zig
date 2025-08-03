479b4dce3c82254714f6c5c8049264f8e47807ce
jvyden <jvyden@jvyden.xyz>
2025-08-03 01:52:10 -0400

    Expose Ipc(De)serializer in Renderite

--- renderite/renderite.zig
+++ renderite/renderite.zig
@@ -1,6 +1,10 @@
 pub const Shared = @import("shared.zig");
 
 const messaging = @import("messaging.zig");
+const serialization = @import("serialization.zig");
 
 pub const QueueManager = messaging.QueueManager;
 pub const MessagingHost = messaging.MessagingHost;
+
+pub const IpcDeserializer = serialization.IpcDeserializer;
+pub const IpcSerializer = serialization.IpcSerializer;
